8a0113f43c6e86a283f61b92e9f936fb8d673cd7
Artur Kvasnii <artur.kvasnii@gmail.com>
2018-04-09 15:23:12 +0300

    Added new aliases

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -112,7 +112,9 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
-      'media': path.resolve(paths.appSrc, 'media')
+      'src': path.resolve(paths.appSrc),
+      'media': path.resolve(paths.appSrc, 'media'),
+      'modules': path.resolve(paths.appSrc, 'modules')
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
