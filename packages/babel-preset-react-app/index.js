646b97a5a6af6299dabf67dd158c2433c979b73c
Artur Kvasnii <artur.kvasnii@gmail.com>
2018-04-23 08:51:44 +0300

    Updated package babel-preset-react-app: - Added supporting of babel-7

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -10,28 +10,28 @@ const plugins = [
   // Necessary to include regardless of the environment because
   // in practice some other transforms (such as object-rest-spread)
   // don't work without it: https://github.com/babel/babel/issues/7215
-  require.resolve('babel-plugin-transform-es2015-destructuring'),
+  require.resolve('@babel/plugin-transform-destructuring'),
   // class { handleClick = () => { } }
-  require.resolve('babel-plugin-transform-class-properties'),
+  require.resolve('@babel/plugin-proposal-class-properties'),
   // The following two plugins use Object.assign directly, instead of Babel's
   // extends helper. Note that this assumes `Object.assign` is available.
   // { ...todo, completed: true }
   [
-    require.resolve('babel-plugin-transform-object-rest-spread'),
+    require.resolve('@babel/plugin-proposal-object-rest-spread'),
     {
       useBuiltIns: true,
     },
   ],
   // Transforms JSX
   [
-    require.resolve('babel-plugin-transform-react-jsx'),
+    require.resolve('@babel/plugin-transform-react-jsx'),
     {
       useBuiltIns: true,
     },
   ],
   // Polyfills the runtime needed for async/await and generators
   [
-    require.resolve('babel-plugin-transform-runtime'),
+    require.resolve('@babel/plugin-transform-runtime'),
     {
       helpers: false,
       polyfill: false,
@@ -49,7 +49,7 @@ const plugins = [
 var env = process.env.BABEL_ENV || process.env.NODE_ENV;
 if (env !== 'development' && env !== 'test' && env !== 'production') {
   throw new Error(
-    'Using `babel-preset-react-app` requires that you specify `NODE_ENV` or ' +
+    'Using `babel-preset-kiaba-react-app` requires that you specify `NODE_ENV` or ' +
       '`BABEL_ENV` environment variables. Valid values are "development", ' +
       '"test", and "production". Instead, received: ' +
       JSON.stringify(env) +
@@ -66,67 +66,71 @@ if (env === 'development' || env === 'test') {
   // https://github.com/facebookincubator/create-react-app/issues/989
   plugins.push.apply(plugins, [
     // Adds component stack to warning messages
-    require.resolve('babel-plugin-transform-react-jsx-source'),
+    require.resolve('@babel/plugin-transform-react-jsx-source'),
     // Adds __self attribute to JSX which React will use for some warnings
-    require.resolve('babel-plugin-transform-react-jsx-self'),
+    require.resolve('@babel/plugin-transform-react-jsx-self'),
   ]);
 }
 
 if (env === 'test') {
-  module.exports = {
-    presets: [
-      // ES features necessary for user's Node version
-      [
-        require('babel-preset-env').default,
-        {
-          targets: {
-            node: 'current',
+  module.exports = function() {
+    return {
+      presets: [
+        // ES features necessary for user's Node version
+        [
+          require('@babel/preset-env').default,
+          {
+            targets: {
+              node: 'current',
+            },
           },
-        },
+        ],
+        // JSX, Flow
+        require.resolve('@babel/preset-react'),
       ],
-      // JSX, Flow
-      require.resolve('babel-preset-react'),
-    ],
-    plugins: plugins.concat([
-      // Compiles import() to a deferred require()
-      require.resolve('babel-plugin-dynamic-import-node'),
-    ]),
+      plugins: plugins.concat([
+        // Compiles import() to a deferred require()
+        require.resolve('babel-plugin-dynamic-import-node'),
+      ]),
+    };
   };
 } else {
-  module.exports = {
-    presets: [
-      // Latest stable ECMAScript features
-      [
-        require.resolve('babel-preset-env'),
-        {
-          targets: {
-            // React parses on ie 9, so we should too
-            ie: 9,
-            // We currently minify with uglify
-            // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-            uglify: true,
+  module.exports = function() {
+    return {
+      presets: [
+        // Latest stable ECMAScript features
+        [
+          require.resolve('@babel/preset-env'),
+          {
+            targets: {
+              // React parses on ie 9, so we should too
+              ie: 9,
+              // We currently minify with uglify
+              // Remove after https://github.com/mishoo/UglifyJS2/issues/448
+              uglify: true,
+            },
+            // Disable polyfill transforms
+            useBuiltIns: false,
+            // Do not transform modules to CJS
+            modules: false,
           },
-          // Disable polyfill transforms
-          useBuiltIns: false,
-          // Do not transform modules to CJS
-          modules: false,
-        },
+        ],
+        // JSX, Flow
+        require.resolve('@babel/preset-react'),
       ],
-      // JSX, Flow
-      require.resolve('babel-preset-react'),
-    ],
-    plugins: plugins.concat([
-      // function* () { yield 42; yield 43; }
-      [
-        require.resolve('babel-plugin-transform-regenerator'),
-        {
-          // Async functions are converted to generators by babel-preset-env
-          async: false,
-        },
-      ],
-      // Adds syntax support for import()
-      require.resolve('babel-plugin-syntax-dynamic-import'),
-    ]),
+      plugins: plugins.concat([
+        // function* () { yield 42; yield 43; }
+        [
+          require.resolve('@babel/plugin-transform-regenerator'),
+          {
+            // Async functions are converted to generators by babel-preset-env
+            async: false,
+          },
+        ],
+        // Adds syntax support for import()
+        require.resolve('@babel/plugin-syntax-dynamic-import'),
+      ]),
+    };
   };
 
   if (env === 'production') {
